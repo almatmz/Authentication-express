1b2851b0bb7ee0a612699ca6f0ede5cb0c9065df
almat <almmuzdubai@gmail.com>
2026-01-28 14:01:09 +0500

    model and controler update

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,37 +1,53 @@
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 exports.register = async (req, res) => {
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-  if (!name || !email || !password)
-    return res.status(400).json({ message: "All fields are required" });
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Please fill all fields" });
+    }
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+    if (!passwordRegex.test(password)) {
+      return res.status(400).json({
+        message:
+          "Password must be at least 8 characters long and include an uppercase letter, a lowercase letter, and a number.",
+      });
+    }
 
-  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
-  if (!passwordRegex.test(password)) {
-    return res.status(400).json({
-      message:
-        "Password must be at least 8 characters long and include an uppercase letter, a lowercase letter, and a number.",
+    const userExists = await User.findOne({ email });
+    if (userExists)
+      return res.status(400).json({ message: "User already exists" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    await User.create({
+      name,
+      email,
+      password: hashedPassword,
     });
+
+    res.status(201).json({ message: "User created!" });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
   }
-
-  const existingUser = await User.findOne({ email });
-  if (existingUser)
-    return res.status(400).json({ message: "Email already registered" });
-
-  await User.create({ name, email, password });
-
-  res.status(201).json({ message: "User registered successfully" });
 };
 
 exports.login = async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
-  if (!user || !(await user.comparePassword(password)))
-    return res.status(401).json({ message: "Invalid credentials" });
+  if (!user)
+    return res.status(401).json({ message: "Invalid email or password" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch)
+    return res.status(401).json({ message: "Invalid email or password" });
 
   req.session.userId = user._id;
-  res.status(200).json({ message: "Login successful" });
+  res.status(200).json({ message: "Welcome!" });
 };
 
 exports.profile = async (req, res) => {
